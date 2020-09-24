fe8bbc04739ccf61013fb631db6f3a46d00fd933
Marisa <mcc170000@utdallas.edu>
2020-09-24 14:11:29 -0500

    Modified the addArguments method to add negative numbers

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,15 +7,23 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide n integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
         int sum = 0;
 	    
-	for (int i = 0 ; i < args.length() ; i++){
-		sum = sum + Integer.valueOf(args[i]);
+	if (args[0] == '-'){
+		for (int i = 1 ; i < args.length() ; i++){
+			sum = sum - Integer.valueOf(args[i]);
+		}	
+	}
+		
+	else{
+		for (int i = 0 ; i < args.length() ; i++){
+			sum = sum + Integer.valueOf(args[i]);
+		}
 	}
 
 	return sum;
